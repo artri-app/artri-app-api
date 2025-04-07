0257d3dd093d31b8cd2609119c1bb54091785a54
Dani-007 <daniel.sobrinho0017@gmail.com>
2025-04-07 20:26:44 -0300

    Add Exercise, Training, and TrainingReport models with multilingual support for days of the week

--- artri_app_api/authentication/models.py
+++ artri_app_api/authentication/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# define the days of the week 
+DAYS_OF_WEEK = [
+        ('Monday', 'Segunda-feira'),
+        ('Tuesday', 'Terça-feira'),
+        ('Wednesday', 'Quarta-feira'),
+        ('Thursday', 'Quinta-feira'),
+        ('Friday', 'Sexta-feira'),
+        ('Saturday', 'Sábado'),
+        ('Sunday', 'Domingo')
+    ]
+
 # Create your models here.
 
 class User(AbstractUser):
@@ -14,15 +25,7 @@ class User(AbstractUser):
         return self.username
     
 class remedy(models.Model):
-    DAYS_OF_WEEK = [
-        ('Monday', 'Segunda-feira'),
-        ('Tuesday', 'Terça-feira'),
-        ('Wednesday', 'Quarta-feira'),
-        ('Thursday', 'Quinta-feira'),
-        ('Friday', 'Sexta-feira'),
-        ('Saturday', 'Sábado'),
-        ('Sunday', 'Domingo')
-    ]
+    
  
     name = models.CharField(max_length=50)
     description = models.TextField()
@@ -34,3 +37,32 @@ class remedy(models.Model):
     def __str__(self):
         return self.name
     
+class Exercise(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    tutorial_link = models.URLField()
+    difficulty = models.Choices(
+        ('Easy', 'Fácil'),
+        ('Medium', 'Médio'),
+        ('Hard', 'Difícil')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Training(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    exercises = models.ManyToManyField(Exercise)
+
+    def __str__(self):
+        return self.name
+    
+class TrainingReport(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trainings = models.ManyToManyField(Training)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.training.name} - {self.date}'
