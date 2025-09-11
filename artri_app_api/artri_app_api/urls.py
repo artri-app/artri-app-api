f09a695a830e9a6bc6eebf2c8d3b53036b1a1e6a
Daniel <daniel.sobrinho0017@gmail.com>
2025-09-11 14:52:56 -0300

    Add missing imports for TokenObtainPairView and TokenRefreshView in URLs

--- artri_app_api/artri_app_api/urls.py
+++ artri_app_api/artri_app_api/urls.py
@@ -20,6 +20,10 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
    openapi.Info(
